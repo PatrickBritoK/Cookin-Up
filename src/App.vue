780bbcf79f255ae5491190ab4927679cb51a46ac
Patrick Brito <patrickbritokun530@gmail.com>
2024-11-19 10:20:01 -0300

    first blood

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+// export default {
+//   components: { Banner }
+// }
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+</template>
+
+<style scoped>
+
+</style>
